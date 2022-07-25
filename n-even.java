7092073b74b0f12b8c8afa1d0fccdde0cbf6abfa
ziaulakram <21mx230@MCA.PSG>
2022-07-25 14:19:02 +0530

    N-even

---
+++ n-even.java
@@ -0,0 +1,9 @@
+class n-even{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        for(int i=1;i<=n;i++){
+            System.out.println(i*2);
+        }
+    }
+}
